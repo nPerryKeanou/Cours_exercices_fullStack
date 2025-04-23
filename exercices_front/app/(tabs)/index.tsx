4760a4cdfba696ab6bb7a0d73b29045df0fc577d
nperryk <nperrykeanou@gmail.com>
2025-04-23 07:11:02 +0200

    ajout de la doc pour l'exercice 04 concernant la navigation des id dans l'url

--- exercices_front/app/(tabs)/index.tsx
+++ exercices_front/app/(tabs)/index.tsx
@@ -1,16 +1,40 @@
+// ---------------------- index de base.
+// import { View, Text, Button } from "react-native";
+// import { useRouter } from 'expo-router';
+
+// export default function Home(){
+// 	const router = useRouter();
+
+// 	return(
+// 		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
+// 		<Text style={{ color: '#000', fontSize: 18 }}>Bienvenue dans les exercices ! 🎉</Text>
+// 		<Button
+// 			title="Aller à l'exercice n°1"
+// 			onPress={() => router.push('/exercices/exercice01')}
+// 		/>
+// 		<Button 
+// 			title="Aller à l'exercice n°2"
+// 			onPress={() => router.push('/exercices/exercice02?user=keanu')}
+// 		/>
+// 	</View>
+// 	);
+// }
+
+// ---------------------- index pour l'exercices 4
+
 import { View, Text, Button } from "react-native";
 import { useRouter } from 'expo-router';
+import ExerciceDetailScreen from "../exercices/[id]";
 
-export default function Home(){
+export default function HomeScreen(){
 	const router = useRouter();
 
 	return(
-		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
-		<Text style={{ color: '#000', fontSize: 18 }}>Bienvenue dans les exercices ! 🎉</Text>
-		<Button
-			title="Aller à l'exercice n°1"
-			onPress={() => router.push('/exercices/exercice01')}
-		/>
-	</View>
+		<view style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
+			<text style={{ marginBottom: 16 }}>Bienvenu dans les exercices.</text>
+			<Button title="Exercice 1" onPress={() => router.push('/exercices/1')}/>
+			<Button title="Exercice 2" onPress={() => router.push('/exercices/2')}/>
+			<Button title="Exercice 3" onPress={() => router.push('/exercices/3')}/>
+		</view>
 	);
 }
---
+++ exercices_front/app/exercices/exercice02.tsx
@@ -0,0 +1,38 @@
+// import { useLocalSearchParams } from 'expo-router';
+// import { View, Text, StyleSheet } from 'react-native';
+
+// export default function ExerciceDetailScreen(){
+//     const { id } = useLocalSearchParams();
+
+//     const renderContent = () => {
+//         switch (id) {
+//             case '1':
+//                 return <Text style={styles.text}>🎯 Exercice 1 : Affichage simple</Text>;
+//             case '2':
+//                 return <Text style={styles.text}>📱 Exercice 2 : Navigation entre écrans</Text>;
+//             case '3':
+//                 return <Text style={styles.text}>🧩 Exercice 3 : Composant personnalisé</Text>;
+//             default:
+//                 return <Text style={styles.text}>❓ Exercice inconnu : {id}</Text>;
+//         }
+//     };
+
+//     return(
+//         <view style={styles.container}>
+//             {renderContent()};
+//         </view>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: '#fff',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//     },
+//     text: {
+//       fontSize: 18,
+//       textAlign: 'center',
+//     },
+//   });
